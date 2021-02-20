62f09de3de9a8e2d4793557aa0bfb8011de3015d
mokny <33606734+mokny@users.noreply.github.com>
2021-02-20 21:13:55 +0100

    Update json.php

--- json.php
+++ json.php
@@ -1,19 +1,28 @@
 <?php
+session_start();
 
-if ($_POST["METHOD"] == "ASD") {
-    //...
+if ($_POST["METHOD"] == "TEST") {
+    //... this is just example data ...
+	$arr['DATA']['test'] = "LAST METHOD ".$_POST['METHOD']." T".time()." ".rand(100,999);
+	$arr['DATA']['ip'] = $_SERVER['REMOTE_ADDR'];
+	$arr['DATA']['time'] = time();
+	$arr['DATA']['uid'] = $_POST['UID'];
+
+	//Adding an Array
+	$arr['DATA']['array'][] = "a";
+	$arr['DATA']['array'][] = "b";
+	
+
+} else {
+	//unknoen method
 }
 
-//Test response
-
-$arr['DATA']['test'] = $_POST['METHOD']."T".time();
-$arr['DATA']['ip'] = $_SERVER['REMOTE_ADDR'];
-
+//Test respons
 
 $arr['RESULT'] = 'SUCCESS';
 $arr['TIMESTAMP'] = time();
-$arr['RAWCONTENT'] = "Bla" . time();
-
+$arr['RAWCONTENT'] = "T-".time();
+$arr['SESSIONID'] = session_id();
 
 echo json_encode($arr);
 ?>
